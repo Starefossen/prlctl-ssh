05a24083b1a56a4a696fa2892da3798dbaf2b4cf
Hans Kristian Flaatten <hans@starefossen.com>
2013-02-03 22:12:52 +0100

    convertet tabs to spaces

--- prlctl-ssh.sh
+++ prlctl-ssh.sh
@@ -3,15 +3,15 @@
 # get status of vm
 # @param $1 vm-name or id
 getVmStatus() {
-	prlctl list -o status $1 | tail -1
-	return 0
+  prlctl list -o status $1 | tail -1
+  return 0
 }
 
 # get ip address of vm
 # @param $1 vm-name or id
 getVmAddr() {
-	prlctl exec $1 ifconfig eth0 | grep "inet addr" | awk -F: '{print $2}' | awk '{print $1}'
-	return 0
+  prlctl exec $1 ifconfig eth0 | grep "inet addr" | awk -F: '{print $2}' | awk '{print $1}'
+  return 0
 }
 
 IFS='@' read -a array <<< "$1"
@@ -21,26 +21,26 @@ vm=${array[1]}
 running=0
 
 while [ $running == 0  ]; do
-	state=`getVmStatus $vm`
-	case "$state" in
-		running )		addr=`getVmAddr $vm`
-						running=1
-						;;
-		suspended ) 	echo "Resuming virtual machine $vm"
-						prlctl resume $vm
-						sleep 5 
-						;;
-		stopped )		echo "Starting virtual machine $vm"
-						prlctl start $vm
-		 				sleep 10
-			 			;;
-		*)				echo "Unknown status $state of virtual machine $vm"
-						running=1;
-						;;
-	esac
+  state=`getVmStatus $vm`
+  case "$state" in
+    running )   addr=`getVmAddr $vm`
+                running=1
+                ;;
+    suspended ) echo "Resuming virtual machine $vm"
+                prlctl resume $vm
+                sleep 5 
+                ;;
+    stopped )   echo "Starting virtual machine $vm"
+                prlctl start $vm
+                sleep 10
+                ;;
+    *)          echo "Unknown status $state of virtual machine $vm"
+                running=1;
+                ;;
+  esac
 done
 
 if [ $addr ]
 then
-	ssh $user@$addr
+  ssh $user@$addr
 fi
